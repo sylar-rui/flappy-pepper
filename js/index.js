75bc05be613f20811c29bebdb16bd97a3ad8b8d6
sylar7 <andy34021@126.com>
2014-03-03 00:21:51 +0800

    Embellish index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(function(){
 	$('.Menu').smint();
+	$('#nav li').children('a').hover(function(){
+		$(this).siblings('span').slideDown(100);
+		var changeColor = this;
+		this.timer = setTimeout(function(){
+		$(changeColor).css({'color':'#fff'});
+	},100);},function(){
+		if(this.timer) clearTimeout(this.timer);
+		$(this).siblings('span').slideUp(100);
+		$(this).css({'color':'#202020'});
+	});
 })
